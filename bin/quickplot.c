6596b2df89fffcdb5b0a9f0241ef7a5fc5290f22
Lance Arsenault <lanceiman@vt.edu>
2026-01-10 12:44:56 -0500

    Saving Work

--- bin/quickplot.c
+++ bin/quickplot.c
@@ -5,6 +5,7 @@
 #include <math.h>
 
 #include <panels.h>
+
 #include "../lib/debug.h"
 
 #include "run.h"
@@ -16,30 +17,14 @@ void catcher(int sig) {
     ASSERT(0, "caught signal number %d", sig);
 }
 
-bool Plot(struct PnWidget *g, struct PnPlot *p, void *userData) {
-
-    ASSERT(userData == (void *) catcher); // testing userData.
+bool Plot(struct PnWidget *g, struct PnPlot *p, void *userData,
+        double xMin, double xMax, double yMin, double yMax) {
 
     const double tMax = 20 * M_PI;
 
     for(double t = 0.0; t <= 2*tMax + 10; t += 0.1) {
         double a = 1.0 - t/tMax;
-        pnGraph_drawPoint(p, a * cos(t), a * sin(t));
-    }
-    return false;
-}
-
-bool Plot2(struct PnWidget *g, struct PnPlot *p, void *userData) {
-
-    ASSERT(userData == 0); // testing userData.
-
-    const double tMax = 20 * M_PI;
-
-    for(double t = 0.02; t <= 2*tMax + 10; t += 0.1) {
-        double a = 1.0 - t/tMax;
-        // This in an inline function, so it can be faster then
-        // if it was not an inline function.
-        pnGraph_drawPoint(p, a * sin(t), a * cos(t));
+        pnPlot_drawPoint(p, a * cos(t), a * sin(t));
     }
     return false;
 }
@@ -70,10 +55,7 @@ int main(void) {
     pnPlot_setLineColor(p, 0xFFFF0000);
     pnPlot_setPointColor(p, 0xFF00FFFF);
     pnPlot_setLineWidth(p, 3.2);
-    pnPlot_setPointSize(p, 12.5);
-
-    p = pnStaticPlot_create(w, Plot2, 0);
-    ASSERT(p);
+    pnPlot_setPointSize(p, 4.5);
 
     pnGraph_setView(w, -1.05, 1.05, -1.05, 1.05);
 
